a746b4a841dee647edd29279e32c0f7a9f97c563
Hudson Kennedy <hudsonhugo@bol.com.br>
2022-05-06 10:20:56 -0300

    remove unused things from example

--- main.js
+++ main.js
@@ -4,7 +4,3 @@ document.querySelector('#app').innerHTML = `
   <h1>Vite Server</h1>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Features</a>
 `
-document.querySelector('#src').innerHTML = `
-  <h2>Vite app</h2>
-  <a href="https://vitejs.dev/guide.html" target="_blank">Guide</a>
-`
